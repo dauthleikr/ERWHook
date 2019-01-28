ebbbdbdc7e57d3ce2b16942325906187bcd68c20
dauthleikr <nino.neuw@gmail.com>
2019-01-28 14:20:37 +0100

    Quick and dirty shellcode implementation, no controls, no disable/dispose

--- ERWHook/ERWHook.h
+++ ERWHook/ERWHook.h
@@ -1,2 +1,17 @@
 #pragma once
+#include <memory>
+#include "byte.h"
 
+class ERWHook
+{
+private:
+	byte* trampoline_;
+	byte* restore_trampoline_;
+	byte* hook_swap_;
+	byte* return_to_;
+
+public:
+	ERWHook(void* target, void* redirect);
+	ERWHook(const ERWHook& copy) = delete;
+	~ERWHook();
+};
--- ERWHookTest/ERWHookTest.cpp
+++ ERWHookTest/ERWHookTest.cpp
@@ -1,20 +1,23 @@
-// ERWHookTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <Windows.h>
+#include "ERWHook.h"
+
+static int MessageBoxHook(HWND hwnd, LPCWSTR text, LPCWSTR caption, UINT type)
+{
+	std::wcout << "MessageBoxW(" << hwnd << ", " << text << ", " << caption << ", " << type << ")" << std::endl;
+	return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	DWORD oldProtect;
+	const auto addr = reinterpret_cast<void*>(MessageBoxW);
+	const auto hook = reinterpret_cast<void*>(MessageBoxHook);
+	VirtualProtect(addr, 0x1000, 0x40, &oldProtect);
+
+	ERWHook erw(addr, hook);
+	
+	MessageBoxW(nullptr, L"Howdy", L"Test", 0);
+	MessageBoxW(nullptr, L"Howdy", L"Test123", 0);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
